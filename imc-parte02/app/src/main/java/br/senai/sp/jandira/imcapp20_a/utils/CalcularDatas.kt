40a4f5e53db4499c8cc2954f213c3a243c5026ca
THAMIRES GAUDENCIO AMÂNCIO <20272597@cfp127aluno.local>
2021-08-20 11:29:49 -0300

    20/08

--- imc-parte02/app/src/main/java/br/senai/sp/jandira/imcapp20_a/utils/CalcularDatas.kt
+++ imc-parte02/app/src/main/java/br/senai/sp/jandira/imcapp20_a/utils/CalcularDatas.kt
@@ -3,19 +3,17 @@ package br.senai.sp.jandira.imcapp20_a.utils
 import android.util.Log
 import java.time.LocalDate
 import java.time.Period
+import java.time.format.DateTimeFormatter
 
 
-fun obterDiferencaEntreDatasEmAnos (dataInicio: String) {
+fun obterDiferencaEntreDatasEmAnos (dataInicio: String)  : String{
 
     var hoje : LocalDate = LocalDate.now()
 
-    var ano = dataInicio.substring(0,4).toInt()
-    var mes = dataInicio.substring(5,7).toInt()
-    var dia = dataInicio.substring(8,10).toInt()
+    var dataIni = LocalDate.parse(dataInicio, DateTimeFormatter.ofPattern("dd/MM/yyyy"))
 
-    var nascimento = LocalDate.of(1974,5,30)
 
-    var idade = Period.between(nascimento,hoje)
-    Log.i("XPTO",idade.years.toString())
+    var idade = Period.between(dataIni,hoje)
+    return idade.years.toString()
 
 }
