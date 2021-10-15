52ce5fe0e94f2eb7ff0e445d5f2775c45a503c00
THAMIRES GAUDENCIO AMÂNCIO <20272597@cfp127aluno.local>
2021-10-15 07:38:30 -0300

    15/10

---
+++ RetrofitViaCep/app/src/main/java/com/example/retrofitviacep/RetrofitService.kt
@@ -0,0 +1,16 @@
+package com.example.retrofitviacep
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+interface RetrofitService {
+    //Método que será responsável por chamar a API
+    // https://viacep.com.br/ws/06600025/json/
+    @GET("{CEP}/json/")
+    fun getCEP(@Path("CEP") cep: String) : Call<Cep>
+
+
+    @POST()
+}
+
